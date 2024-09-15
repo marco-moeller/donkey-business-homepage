97a9f04bef344aae38fd430ab4aae8eeef736fec
Marco Moeller <maaakooo@gmx.de>
2024-09-15 15:28:07 +0200

    feature: tooltip follow mouse
    the tooltip component now follows mouse movements

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
 import useVisibilityToggle from "../../hooks/useVisibilityToggle";
 
 function Tooltip({ children, text = "Tooltip" }) {
   const { isShowing, toggle } = useVisibilityToggle();
+  const [position, setPosition] = useState([0, 0]);
+
+  const setTooltipPosition = (event) => {
+    setPosition([event.pageX - window.scrollX, event.pageY - window.scrollY]);
+  };
 
   return (
     <div
       className="tooltip--children"
       onMouseEnter={toggle}
       onMouseLeave={toggle}
+      onMouseMove={setTooltipPosition}
     >
-      {isShowing && <div className="tooltip">{text}</div>}
+      {isShowing && (
+        <div
+          className="tooltip"
+          style={{
+            left: position[0],
+            top: position[1]
+          }}
+        >
+          {text}
+        </div>
+      )}
       {children}
     </div>
   );
