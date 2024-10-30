92be4b74ac26fc745f750595adde0879d88dfbd7
Marco Moeller <maaakooo@gmx.de>
2024-10-30 17:39:20 +0100

    moved firebase config to .env file
    firebase config no longer public, but in private .env file

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -2,17 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore, collection } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDQzzPdfSpiwv7SJvBkJlfc0f4tMrQ1x1o",
-  authDomain: "donkey-business-9bd22.firebaseapp.com",
-  projectId: "donkey-business-9bd22",
-  storageBucket: "donkey-business-9bd22.appspot.com",
-  messagingSenderId: "285067581032",
-  appId: "1:285067581032:web:492c15ee75c1d133218ddd"
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
 
